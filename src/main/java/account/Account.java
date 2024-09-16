f9c78c40eb595f5a82383ad3ed9f675cd5805390
Aleexender <alexkjh2011@gmail.com>
2024-09-16 21:24:03 +0900

    init commit

---
+++ src/main/java/account/Account.java
@@ -0,0 +1,37 @@
+package account;
+
+
+public class Account {
+    private final String name;
+    private final int age;
+    private long asset;
+
+    public Account(String name, int age, long asset) {
+        this.name = name;
+        this.age = age;
+        this.asset = asset;
+    }
+
+    public long deposit(long amount) {
+        return asset += amount;
+    }
+
+    public long withdraw(long amount) {
+        if (this.asset < amount) {
+            throw new IllegalArgumentException("low asset");
+        }
+        return asset -= amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public long getAsset() {
+        return asset;
+    }
+}
