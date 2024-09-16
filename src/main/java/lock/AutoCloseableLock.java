d4d96ea977dca2f7ae6adf43b86aa3300a879df9
Aleexender <alexkjh2011@gmail.com>
2024-09-16 22:07:55 +0900

    implement autoCloseable for try with resource

---
+++ src/main/java/lock/AutoCloseableLock.java
@@ -0,0 +1,17 @@
+package lock;
+
+import java.util.concurrent.locks.Lock;
+
+public class AutoCloseableLock implements AutoCloseable {
+    private final Lock lock;
+
+    public AutoCloseableLock(Lock lock) {
+        this.lock = lock;
+        this.lock.lock();
+    }
+
+    @Override
+    public void close() {
+        lock.unlock();
+    }
+}
